777bb77f1aebd083fffeefc65273d042bedaaf78
JDG <jorgengreve80@gmail.com>
2025-05-11 04:49:43 +0200

    Initial commit

---
+++ src/gps.cpp
@@ -0,0 +1,125 @@
+#define TINY_GSM_MODEM_SIM7000
+// Set serial for debug console (to the Serial Monitor, default speed 115200)
+#define SerialMon Serial
+
+#include "gps.h"
+#include <Arduino.h>
+#include <TinyGsmClient.h>
+
+
+#define SerialAT        Serial1
+#define PWR_PIN         4
+#define LED_PIN         12
+
+#ifdef DUMP_AT_COMMANDS
+#include <StreamDebugger.h>
+StreamDebugger debugger(SerialAT, SerialMon);
+TinyGsm modem(debugger);
+#else
+TinyGsm modem(SerialAT);
+#endif
+
+
+
+void enableGPS(void)
+{
+    // Set Modem GPS Power Control Pin to HIGH ,turn on GPS power
+    // Only in version 20200415 is there a function to control GPS power
+    modem.sendAT("+CGPIO=0,48,1,1");
+    if (modem.waitResponse(10000L) != 1) {
+        DBG("Set GPS Power HIGH Failed");
+    }
+    modem.enableGPS();
+}
+
+void disableGPS(void)
+{
+    // Set Modem GPS Power Control Pin to LOW ,turn off GPS power
+    // Only in version 20200415 is there a function to control GPS power
+    modem.sendAT("+CGPIO=0,48,1,0");
+    if (modem.waitResponse(10000L) != 1) {
+        DBG("Set GPS Power LOW Failed");
+    }
+    modem.disableGPS();
+}
+
+void modemPowerOn()
+{
+    pinMode(PWR_PIN, OUTPUT);
+    digitalWrite(PWR_PIN, HIGH);
+    delay(1000);    //Datasheet Ton mintues = 1S
+    digitalWrite(PWR_PIN, LOW);
+}
+
+void modemPowerOff()
+{
+    pinMode(PWR_PIN, OUTPUT);
+    digitalWrite(PWR_PIN, HIGH);
+    delay(1500);    //Datasheet Ton mintues = 1.2S
+    digitalWrite(PWR_PIN, LOW);
+}
+
+
+void modemRestart()
+{
+    modemPowerOff();
+    delay(1000);
+    modemPowerOn();
+}
+
+
+void getGps()
+{
+     if (!modem.testAT()) {
+        Serial.println("Failed to restart modem, attempting to continue without restarting");
+        modemRestart();
+        return;
+    }
+
+    Serial.println("Start positioning . Make sure to locate outdoors.");
+    Serial.println("The blue indicator light flashes to indicate positioning.");
+
+    enableGPS();
+
+    float lat,  lon, speed;
+    while (1) {
+        if (modem.getGPS(&lat, &lon, &speed)) {
+            Serial.println("The location has been locked, the latitude and longitude are:");
+            Serial.print("latitude: "); Serial.println(lat, 10);
+            Serial.print("longitude:"); Serial.println(lon, 10);
+            Serial.print("Speed:    "); Serial.print(speed, 10); Serial.println("km/h");
+            break;
+        }
+        digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+        delay(2000);
+    }
+
+    disableGPS();
+
+    Serial.println("/**********************************************************/");
+    Serial.println("After the network test is complete, please enter the  ");
+    Serial.println("AT command in the serial terminal.");
+    Serial.println("/**********************************************************/\n\n");
+
+    while (1) {
+        while (SerialAT.available()) {
+            SerialMon.write(SerialAT.read());
+        }
+        while (SerialMon.available()) {
+            SerialAT.write(SerialMon.read());
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/gps.h
@@ -0,0 +1,16 @@
+#ifndef GPS_H
+#define GPS_H
+
+#include <TinyGsmClient.h>
+
+
+void enableGPS(void);
+void disableGPS(void);
+void modemPowerOn();
+void modemPowerOff();
+void modemRestart();
+void getGps();
+
+
+
+#endif
